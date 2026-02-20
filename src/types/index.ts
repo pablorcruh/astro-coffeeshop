bce2b3ab85f6dbde04fc19ada0504d9eb3c97ab4
pablorcruh <pablorcruh@gmail.com>
2026-02-19 23:03:11 -0500

    add zod schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,20 @@
 import { z } from 'astro:content'
 
+
+const imageSchema  = z.object({
+    url: z.string(),
+    width: z.number(),
+    height: z.number()
+})
+
+const featuredImagesSchema = z.object({
+    thumbnail: imageSchema,
+    medium: imageSchema,
+    medium_large: imageSchema,
+    large: imageSchema,
+    full: imageSchema
+})
+
 export const BaseWPSSchema = z.object({
     id: z.number(),
     title: z.object({
@@ -8,6 +23,7 @@ export const BaseWPSSchema = z.object({
     content: z.object({
         rendered: z.string()
     }),
+    featured_images: featuredImagesSchema,
     acf: z.object({
         subtitle: z.string()
     })
